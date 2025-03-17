974e7d5e9b7198f1ab711b2e007619af401e7679
David Promontor Farias <dpromontor@imperquimia.com.mx>
2025-03-17 10:06:16 -0600

    Cambios en el servidor

--- src/main.js
+++ src/main.js
@@ -93,7 +93,7 @@ document.addEventListener('DOMContentLoaded', function () {
       });
 
       // Enviar correo electrónico al supervisor
-      const response = await fetch('http://localhost:3000/send-email', {
+      const response = await fetch('https://backendimpervacaciones.onrender.com/send-email', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
